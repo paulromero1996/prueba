ea820bcd597476df837e42c7435e5db861b3d38b
paulromero1996 <66227622+paulromero1996@users.noreply.github.com>
2021-09-30 10:55:41 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,18 +1,35 @@
 
+var1=" ";
 
-var1="";
-function historial1() {
-	//alert("led on");
-	console.log("mostrando historial");
-	message = new Paho.MQTT.Message("historial");
-    	message.destinationName = "prromero.fis@unach.edu.ec/prueba1";
-    	client.send(message);
-  	var1="h1";
+
+function LED1_On(){
+	message = new Paho.MQTT.Message("SENSOR1");
+	message.destinationName = "prromero.fis@unach.edu.ec/prueba1";
+	client.send(message); 
+	console.log("MUESTRA SENSOR 1");
+	//var1="H1";
+}
+
+function LED2_On(){	
+	message = new Paho.MQTT.Message("SENSOR2");
+	message.destinationName ="prromero.fis@unach.edu.ec/prueba1";
+	client.send(message);
+	console.log("MUESTRA SENSOR 2");
+	//var2="H2";
 }
 
 
+function Exportar1(){	
+	message = new Paho.MQTT.Message("HISTORIAL");
+	message.destinationName ="prromero.fis@unach.edu.ec/prueba1";
+	client.send(message);
+	console.log("MUESTRA HISTORIAL");
+	var1="H1";
+}
 
 
+// Create a client instance
+  //client = new Paho.MQTT.Client("postman.cloudmqtt.com", 14970);
   
   client = new Paho.MQTT.Client("maqiatto.com", 8883, "web_" + parseInt(Math.random() * 100, 10));
 
@@ -35,9 +52,8 @@ function historial1() {
     // Once a connection has been made, make a subscription and send a message.
     console.log("Conectado...");
 	
-    client.subscribe("prromero.fis@unach.edu.ec/prueba1");	  
     client.subscribe("prromero.fis@unach.edu.ec/prueba");
-    message = new Paho.MQTT.Message("---");
+    message = new Paho.MQTT.Message("CONECTADO");
     message.destinationName = "prromero.fis@unach.edu.ec/prueba1";
     client.send(message);
 	
@@ -57,18 +73,27 @@ function historial1() {
 
 
   function onMessageArrived(message) {
-	  mensaje=message.payloadString;
-	  vector=mensaje.split(";");
-	  tamano=vector.length;
-	  document.getElementById("sensor").innerHTML=vector[0];
-	  document.getElementById("sensor2").innerHTML=vector[1];
+
 	  
-	  if(tamano>=3 && var1=="h1"){
-	  document.getElementById("historial11").innerHTML=vector[2];
+	mensaje=(message.payloadString);
+	datos=mensaje.split(";")  
+	lon =datos.length;
+	  
+	  if(mensaje=="Alta"||mensaje=="Baja"){
+	  	document.getElementById("sensor1").innerHTML=mensaje;
+	  }
+	  if(mensaje=="Vacio"||mensaje=="Lleno"){
+	  	document.getElementById("sensor2").innerHTML=mensaje;	  
+	  }
+	  
+	  if(lon==2&&var1=="H1"){
+		document.getElementById("historial1").innerHTML=datos[0];
+	  	document.getElementById("historial2").innerHTML=datos[1];
 	  }
 
-	  
-	  
+	
 
- }
-  
+	  
+	  
+	  
+  }
